03534672dd426798eb11439f5eda404916004fc9
siwuxie095 <834879583@qq.com>
2020-10-08 18:31:53 +0800

    初始化；

---
+++ src/com/siwuxie095/functional/chapter1st/example1st/Main.java
@@ -0,0 +1,13 @@
+package com.siwuxie095.functional.chapter1st.example1st;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-10-08 18:31:34
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}

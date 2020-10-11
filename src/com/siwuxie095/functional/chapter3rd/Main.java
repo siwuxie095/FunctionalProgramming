f13c2da525d09b759b8bc8c77b2daeac6beaf560
siwuxie095 <834879583@qq.com>
2020-10-11 18:49:38 +0800

    流；

---
+++ src/com/siwuxie095/functional/chapter3rd/Main.java
@@ -0,0 +1,20 @@
+package com.siwuxie095.functional.chapter3rd;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-10-11 17:40:37
+ */
+public class Main {
+
+    /**
+     * 流
+     *
+     * Java 8 中新增的特性旨在帮助程序员写出更好的代码，其中对核心类库的改进是很关键的
+     * 一部分。对核心类库的改进主要包括集合类的 API 和新引入的流 (Stream)。流使程序员
+     * 得以站在更高的抽象层次上对集合进行操作。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}

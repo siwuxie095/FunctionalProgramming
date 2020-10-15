13f42db1fe90917ad8310c4ff4d9b465d2f43e21
siwuxie095 <834879583@qq.com>
2020-10-15 22:26:10 +0800

    类库；

---
+++ src/com/siwuxie095/functional/chapter4th/example1st/Main.java
@@ -0,0 +1,25 @@
+package com.siwuxie095.functional.chapter4th.example1st;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-10-15 21:52:38
+ */
+public class Main {
+
+    /**
+     * 􏰁􏰂􏰁􏰂类库
+     * 􏰁􏰂􏰁􏰂
+     * 这里将会介绍如何使用 Lambda 表达式。即使不需要编写像 Stream 这样重度使用函数式编程风格的类库，学会如何
+     * 使用 Lambda 表达式也是非常重要的。即使一个最简单的应用，也可能会因为代码即数据的函数式编程风格而受益。
+     *
+     * Java 8 中的另一个变化是引入了默认方法和接口的静态方法，它改变了程序员认识类库的方式，接口中的方法也可以
+     * 包含代码体了。
+     *
+     * 另外还会介绍 Lambda 表达式方法重载的工作原理、基本类型的使用方法等。使用 Lambda 表达式编写程序时，掌握
+     * 这些知识非常重要。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}

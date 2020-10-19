ac303f58e4be7da2f69ff58337a49c29d1ecdebf
siwuxie095 <834879583@qq.com>
2020-10-19 08:33:50 +0800

    高级集合类和收集器；

---
+++ src/com/siwuxie095/functional/chapter5th/example1st/Main.java
@@ -0,0 +1,21 @@
+package com.siwuxie095.functional.chapter5th.example1st;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-10-19 08:30:08
+ */
+public class Main {
+
+    /**
+     * 􏰇􏰈􏰉􏰊􏰁􏰋􏰌􏰉􏰍􏰇􏰈􏰉􏰊􏰁􏰋􏰌􏰉高级集合类和收集器
+     *
+     * 这里将介绍 Java 8 中集合类的一些高级主题了，包括新引入的 Collector 类。同时还会介绍方法引用，它可以帮助程序员
+     * 在 Lambda 表达式中轻松使用已有代码。编写大量使用集合类的代码时，使用方法引用能让程序员获得丰厚的回报。
+     *
+     * 另外还会涉及集合类的一些更高级的主题，比如流中元素的顺序，以及一些有用的 API。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}

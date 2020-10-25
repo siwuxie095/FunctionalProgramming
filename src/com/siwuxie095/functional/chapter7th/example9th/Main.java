9b503589c583150ce028151b745b94eb5239f0b2
siwuxie095 <834879583@qq.com>
2020-10-25 16:04:07 +0800

    要点回顾；

---
+++ src/com/siwuxie095/functional/chapter7th/example9th/Main.java
@@ -0,0 +1,20 @@
+package com.siwuxie095.functional.chapter7th.example9th;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-10-25 16:02:24
+ */
+public class Main {
+
+    /**
+     * 要点回顾
+     *
+     * （1）重构遗留代码时考虑如何使用 Lambda 表达式，有一些通用的模式。
+     * （2）如果想要对复杂一点的 Lambda 表达式编写单元测试，将其抽取成一个常规的方法。
+     * （3）peek 方法能记录中间值，在调试时非常有用。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}

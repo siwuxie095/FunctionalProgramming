fe765fd37e531fb35c60ea2605811787661e14a8
siwuxie095 <834879583@qq.com>
2020-10-25 15:21:01 +0800

    惰性求值和调试；

---
+++ src/com/siwuxie095/functional/chapter7th/example5th/Main.java
@@ -0,0 +1,22 @@
+package com.siwuxie095.functional.chapter7th.example5th;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-10-25 15:17:37
+ */
+public class Main {
+
+    /**
+     * 惰性求值和调试
+     *
+     * 调试时通常会设置断点，单步跟踪程序的每一步。使用流时，调试可能会变得更加复杂，因为迭代已交由类库控制，而且
+     * 很多流操作是惰性求值的。
+     *
+     * 在传统的命令式编程看来，代码就是达到某种目的的一系列行动，在行动前后查看程序状态是有意义的。在 Java 8 中，
+     * 仍然可以使用 IDE 提供的各种调试工具，但有时需要调整实现方式，以期达到更好的结果。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}

e0008d7ef672a55cd0dba433ca756b89b85b16e9
siwuxie095 <834879583@qq.com>
2020-10-26 08:24:29 +0800

    使用 Lambda 表达式的 SOLID 原则；

---
+++ src/com/siwuxie095/functional/chapter8th/example8th/Main.java
@@ -0,0 +1,29 @@
+package com.siwuxie095.functional.chapter8th.example8th;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-10-26 08:12:04
+ */
+public class Main {
+
+    /**
+     * 使用 Lambda 表达式的 SOLID 原则
+     *
+     * SOLID 原则是设计面向对象程序时的一些基本原则。原则的名字是个简写，分别代表了下面五个词的首字母：
+     * （1）Single responsibility     单一功能原则（单一职责原则）
+     * （2）Open/closed               开闭原则（开放封闭原则）
+     * （3）Liskov substitution       里氏替换原则
+     * （4）Interface segregation     接口隔离原则
+     * （5）Dependency inversion      依赖反转原则（依赖倒置原则）
+     *
+     * 这些原则能指导程序员开发出易于维护和扩展的代码。
+     *
+     * 每种原则都对应着一系列潜在的代码异味，并为其提供了解决方案。有很多图书介绍这个主题，因此这里不会
+     * 详细讲解，而是关注如何在 Lambda 表达式的环境下应用其中的三条原则，分别是（1）、（2）、（5）。
+     * 在 Java 8 中，有些原则通过扩展，已经超出了原来的限制。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}

db665d50a27f43b4b890098df7e6a0607a51c69f
siwuxie095 <834879583@qq.com>
2020-10-27 21:28:49 +0800

    要点回顾；

---
+++ src/com/siwuxie095/functional/chapter8th/example12th/Main.java
@@ -0,0 +1,24 @@
+package com.siwuxie095.functional.chapter8th.example12th;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-10-27 21:23:50
+ */
+public class Main {
+
+    /**
+     * 要点回顾
+     *
+     * （1）Lambda 表达式能让很多现有设计模式更简单、可读性更强，尤其是命令者模式。
+     * （2）在 Java 8 中，创建领域专用语言有更多的灵活性。
+     * （3）在 Java 8 中，有应用 SOLID 原则的新机会。
+     *
+     * PS：对于设计原则，可以参考如下链接中的设计模式主题。
+     *
+     * http://www.objectmentor.com/resources/publishedArticles.html
+     */
+    public static void main(String[] args) {
+
+    }
+
+}

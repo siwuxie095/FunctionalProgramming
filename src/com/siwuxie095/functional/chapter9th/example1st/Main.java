30381c0dd48216e69534e3ef1287e886f25b8221
siwuxie095 <834879583@qq.com>
2020-10-27 21:39:00 +0800

    使用 Lambda 表达式编写并发程序；

---
+++ src/com/siwuxie095/functional/chapter9th/example1st/Main.java
@@ -0,0 +1,21 @@
+package com.siwuxie095.functional.chapter9th.example1st;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-10-27 21:30:15
+ */
+public class Main {
+
+    /**
+     * 使用 Lambda 表达式编写并发程序
+     *
+     * 这里将讨论如何使用 Lambda 表达式编写并发应用，高效传递信息和非阻塞式 I/O。
+     *
+     * 这里的一些例子用到了 Vert.x（http://vertx.io/）和 RxJava（https://github.com/ReactiveX/RxJava）框架，
+     * 但其中展现的设计原则是通用的，对其他框架或是自己编写的、没有使用任何框架的程序也适用。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
